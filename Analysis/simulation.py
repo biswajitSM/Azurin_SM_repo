e271473c21148680301358ec7c08a527ca2d7623
Biswajit Pradhan <biswajitp145@gmail.com>
2017-10-19 18:06:14 +0200

    saving simulated data

--- Analysis/simulation.py
+++ Analysis/simulation.py
@@ -1,5 +1,7 @@
+import os
 import numpy as np
 import pandas as pd
+import h5py
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 def simulate_on_off_times(ton1=0.01, ton2=0.002, toff1=0.250, toff2=0.005, time_len=60, plotting=False):
@@ -25,21 +27,25 @@ def simulate_on_off_times(ton1=0.01, ton2=0.002, toff1=0.250, toff2=0.005, time_
     numpoints = int(time_len/(ton1+toff1));
     print('numpoint: '+str(numpoints))
     ontimes_exp_1 = np.random.choice(tons_pdf, numpoints, p=pdf_on_exp);
+    ontimes_exp_1 = np.round(ontimes_exp_1, 4);
     ontimes_exp_rise = np.random.choice(tons_pdf, numpoints, p=pdf_on_rise);
+    ontimes_exp_rise = np.round(ontimes_exp_rise, 4);
     offtimes_exp_1 = np.random.choice(toffs_pdf, numpoints, p=pdf_off_exp);
+    offtimes_exp_1 = np.round(offtimes_exp_1, 4);
     offtimes_exp_rise = np.random.choice(toffs_pdf, numpoints, p=pdf_off_rise);
+    offtimes_exp_rise = np.round(offtimes_exp_rise, 4);
     if plotting == True:
         bins_default = 100;
         fig = plt.figure(figsize=(20, 10))
         nrows=3; ncols=2;
-        ax00 = plt.subplot2grid((nrows, ncols), (0,0))
+        ax00 = plt.subplot2grid((nrows, ncols), (0,0)) 
         ax01 = plt.subplot2grid((nrows, ncols), (0,1))
         ax10 = plt.subplot2grid((nrows, ncols), (1,0))
         ax11 = plt.subplot2grid((nrows, ncols), (1,1))
         ax20 = plt.subplot2grid((nrows, ncols), (2,0))
         ax21 = plt.subplot2grid((nrows, ncols), (2,1))
         #ontimes exponential
-        time = np.cumsum(ontimes_exp_1+offtimes_exp_1)
+        time = np.cumsum(ontimes_exp_1 + offtimes_exp_1)
         ax00.plot(time, ontimes_exp_1, label='ontimes exponential')
         time = np.cumsum(ontimes_exp_rise+offtimes_exp_rise)
         ax10.plot(time, ontimes_exp_rise, label='ontimes risetime')
@@ -114,8 +120,63 @@ def timestamps_from_onofftrace(ontimes, offtimes,
         plt.figure()
         plt.plot(t_int_on_pdf, pdf_int_on, '.')
         plt.plot(t_int_off_pdf, pdf_int_off, '.')
-
+    timestamps = np.round(timestamps, 8);
     return timestamps, timestamps_marker
+def save_simulated_trace(ton1=0.016, ton2=0.002, toff1=0.250,
+                    toff2=0.02, time_len=10, 
+                    i_on_mu=3000, i_off_mu=200, 
+                    allcomb=False):
+    '''
+    time traces are simulated with photon arrival times
+    all the necessary information are saved in a hdf5 file'''
+    #getting name of data folder and creating data folder if does't exist 
+    data_folder_sim = 'data_simulated'
+    if os.path.isdir(data_folder_sim):
+        data_folder_sim = os.path.abspath(data_folder_sim)
+    else:
+        data_folder_sim = os.makedirs(data_folder_sim)
+        data_folder_sim = os.getcwd();
+    #creating hdf5 file name where data will be saved
+    import datetime
+    date = datetime.datetime.today().strftime('%Y%m%d_%H%M')
+    hdf5_tosave = os.path.join(data_folder_sim, 
+                              date+'_ton'+str(ton1)+'toff'+str(toff1)+'timelen'+str(time_len)
+                              +'.hdf5');
+    f_saveHDF5 = h5py.File(hdf5_tosave, "w");#create or open hdf5 file in write mode
+    #save all the parameters used in the simulation
+    f_saveHDF5['parameters/ton1'] = ton1;
+    f_saveHDF5['parameters/ton2'] = ton2;
+    f_saveHDF5['parameters/toff1'] = toff1;
+    f_saveHDF5['parameters/toff2'] = toff2;
+    f_saveHDF5['parameters/counts_on'] = i_on_mu;
+    f_saveHDF5['parameters/counts_off'] = i_off_mu;
+    #Simulate array of on and off times
+    out = simulate_on_off_times(ton1, ton2, toff1,
+                                toff2, time_len, plotting=False);
+    [ontimes_exp_1, ontimes_exp_rise, offtimes_exp_1, offtimes_exp_rise] = out
+    # simulate for ontime_exp_rise and offtime_exp_rise;
+    grp_rise = f_saveHDF5.create_group('onrise_offrise');
+    grp_rise.create_dataset('ontimes_exp_rise', data=ontimes_exp_rise);
+    grp_rise.create_dataset('offtimes_exp_rise', data=offtimes_exp_rise);
+    out_ts = timestamps_from_onofftrace(ontimes_exp_rise, offtimes_exp_rise, 
+                                     i_on_mu, i_off_mu);
+    [timestamps, timestamps_marker] = out_ts;
+    grp_rise.create_dataset('timestamps', data=timestamps);
+    grp_rise.create_dataset('timestamps_marker', data=timestamps_marker, dtype='int8');
+    f_saveHDF5.flush()
+    # simulate for ontime_exp1 and offtime_exp_1
+    grp_exp = f_saveHDF5.create_group('onexp_offexp');
+    grp_exp.create_dataset('ontimes_exp', data=ontimes_exp_1);
+    grp_exp.create_dataset('offtimes_exp', data=offtimes_exp_1);
+    out_ts = timestamps_from_onofftrace(ontimes_exp_1, offtimes_exp_1, 
+                                     i_on_mu, i_off_mu);
+    [timestamps, timestamps_marker] = out_ts;
+    grp_exp.create_dataset('timestamps', data=timestamps);
+    grp_exp.create_dataset('timestamps_marker', data=timestamps_marker, dtype='int8');
+    f_saveHDF5.flush()
+    # close hdf5 and end of func
+    f_saveHDF5.close()
+    return hdf5_tosave
 def timetrace_from_onofftrace(ontimes, offtimes, bintime=1e-3,
                              i_on_mu=2000, i_on_sig=1000,
                              i_off_mu=200, i_off_sig=240,
@@ -185,7 +246,7 @@ def intensity_fit(phtoton_arrtimes, bintime):
     tt_length = round(tt_length, 0);
     binpts=tt_length/bintime;
     hist, trace = np.histogram(df, bins=binpts, range=(min(df), max(df)));
-    count_hist, counts = np.histogram(hist, bins=100);
+    count_hist, counts = np.histogram(hist*1e-3/bintime, bins=100);
     #=======plotting==========
     fig = plt.figure(figsize=(16, 8));
     nrows=2; ncols=2;
@@ -193,11 +254,14 @@ def intensity_fit(phtoton_arrtimes, bintime):
     ax01 = plt.subplot2grid((nrows, ncols), (0,1));
     ax10 = plt.subplot2grid((nrows, ncols), (1,0), colspan=2)
 
-    ax00.plot(trace[:-1], hist);
+    ax00.plot(trace[:-1], hist*1e-3/bintime);
+    ax00.set_ylabel('counts/kcps')
+    ax00.set_xlabel('time/s')
     ax00.set_title('bintime: ' + str(bintime))
-    ax01.bar(counts[:-1], count_hist/sum(count_hist)    )
+    ax01.bar(counts[:-1], count_hist, width=0.1)
     # mu1, sig1, mu2, sig2 = two_gaussian_fit(ax01, counts[:-1], count_hist)
     ax01.set_yscale('log')
+    ax01.set_xlabel('counts/kcps')    
     # ax01.set_title('mu1: '+str(np.round(mu1, 1)) +
     #               '    sig1: '+str(np.round(sig1, 1)) + '\n'
     #                 'mu2: '+str(np.round(mu2, 1)) +
